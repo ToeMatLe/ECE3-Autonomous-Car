f5c170cba551d3c872eaa56334e3561f7589d53b
Thomas Le <142299866+ToeMatLe@users.noreply.github.com>
2025-11-23 02:14:31 -0800

    Final track

---
+++ Final.ino
@@ -0,0 +1,210 @@
+#include <ECE3.h>
+uint16_t sensorValues[8]; // right -> left, 0 -> 7
+
+// Motor Constants
+// left side
+const int left_nslp_pin=31; 
+const int left_dir_pin=29;
+const int left_pwm_pin=40;
+// right side
+const int right_nslp_pin=11; 
+const int right_dir_pin=30;
+const int right_pwm_pin=39;
+
+//light debugg pins
+const int yellowled = 51;
+
+// Minimum Constants
+const int min[8] = {734, 619, 664, 596, 619, 664, 711, 734};
+// Maximum Constants
+const int max[8] = {1766, 1881, 1836, 1904, 1881, 1836, 1789, 1766};
+
+// IR Emitters Constants
+const int IR_LED_odd = 45;
+const int IR_LED_even = 61;
+
+//Base Speed
+int leftSpd = 0;
+int rightSpd = 0;
+int baseSpeed = 40;
+
+// PID (arbutarity 50)
+// if kp is negative, speed left motor, slow down right motor
+int kp = 600;
+int kd = 12;
+int prevError = 0;
+
+// Define the weighting of the pins using (15-14-12-8)/8 weighting
+// Thought Process is that you add up the photopin values, left as negative, right as positive
+
+// slow down speed, more room for correction so all 4 starting positions will work
+int photoWeight[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
+
+// Variables to caclulate error in sensor fusion
+int error;
+int sensorSum;
+int result; 
+
+bool isOnLine();
+int lineCount = 0;
+int phantomDetect = 0;
+
+int TURN_TIME = 505;  // ms you measured for ~225°
+bool turning = false;
+int turnStart = 0;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  ECE3_Init();
+// left
+  pinMode(left_nslp_pin,OUTPUT);
+  pinMode(left_dir_pin,OUTPUT);
+  pinMode(left_pwm_pin,OUTPUT);
+  digitalWrite(left_dir_pin,LOW);
+  digitalWrite(left_nslp_pin,HIGH);
+// right
+  pinMode(right_nslp_pin,OUTPUT);
+  pinMode(right_dir_pin,OUTPUT);
+  pinMode(right_pwm_pin,OUTPUT);
+  digitalWrite(right_dir_pin,LOW);
+  digitalWrite(right_nslp_pin,HIGH);  
+// ir emmiters
+  pinMode(IR_LED_odd, OUTPUT);
+  pinMode(IR_LED_even, OUTPUT);
+  digitalWrite(IR_LED_odd, HIGH);
+  digitalWrite(IR_LED_even, HIGH);
+  
+//LED 
+  pinMode(yellowled, OUTPUT);
+  digitalWrite(yellowled, LOW);
+
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() {
+  ECE3_read_IR(sensorValues);
+  // for (int i = 0; i < 8; i++) {
+  //   // result = sensorValues[i] - min[i];
+  //   // result *= 1000;
+  //   // result /= max[i];
+  //   Serial.print(sensorValues[i]);
+  //   Serial.print(", ");
+  // }
+  // Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  // delay(1000);
+  
+  result = 0;
+  error = 0;
+  sensorSum = 0;
+  for (int i = 0; i < 8; i++) {
+    result = sensorValues[i] - min[i];
+    result *= 1000;
+    result /= max[i];
+    if (result < 0) result = 0;
+    result *= photoWeight[i];
+    sensorSum += result;
+  }
+  error = sensorSum/8;
+  // Serial.println(error);
+  // Serial.println();
+  // delay(100); 
+
+  int diffSum = error - prevError;
+  int pidSum = kp*error/100000 + kd*diffSum/10000;
+
+  leftSpd = baseSpeed - pidSum;
+  rightSpd = baseSpeed + pidSum;
+  analogWrite(left_pwm_pin,leftSpd);
+  analogWrite(right_pwm_pin,rightSpd); 
+  
+  prevError = error;
+  
+// If we are currently turning, keep turning until TURN_TIME elapsed
+  if (turning && lineCount == 1) {
+    if (millis() - turnStart < TURN_TIME) {
+      // spin in place: left backward, right forward
+      digitalWrite(left_dir_pin, HIGH);   // adjust if your wiring is opposite
+      digitalWrite(right_dir_pin, LOW);
+
+      analogWrite(left_pwm_pin,  150);
+      analogWrite(right_pwm_pin, 150);
+      return; // skip rest of loop while turning
+    } else {
+      digitalWrite(left_dir_pin, LOW);  
+      digitalWrite(right_dir_pin, LOW);
+      //Give right weighting less power
+      // COULD TRY, Make weight less instead of increase
+      photoWeight[0] = -12;
+      photoWeight[1] = -12;
+      photoWeight[2] = -12;
+      photoWeight[3] = -15;
+      photoWeight[4] = 15;
+      photoWeight[5] = 14;
+      photoWeight[6] = 20;
+      photoWeight[7] = 24;
+      turning = false;
+    }
+  }
+
+  // If we're NOT already turning, check for line hit
+  if (!turning && isOnLine()) {
+  // stop briefly on the line
+    analogWrite(left_pwm_pin, 0);
+    analogWrite(right_pwm_pin, 0);
+    delay(1000);
+
+    isOnline() ? phantomDetect += 1 : phantomDetect = 0;
+
+    // start turn
+    if (phantomDetect >= 2 && lineCount == 0){ //225 degree trun
+      lineCount += 1;
+      phantomDetect = 0;
+      turning = true;
+      turnStart = millis();
+    } else if(phantomDetect >= 2 && lineCount == 1) { // Begin timed run
+      // Change weights back to normal, light turn on to check
+      phantomDetect = 0;
+      lineCount += 1;
+      // time starts, give right weighting
+      photoWeight[0] = -18;
+      photoWeight[1] = -15;
+      photoWeight[2] = -12;
+      photoWeight[3] = -9;
+      photoWeight[4] = 9;
+      photoWeight[5] = 12;
+      photoWeight[6] = 14;
+      photoWeight[7] = 15;
+      digitalWrite(yellowled, HIGH);
+      baseSpeed = 30;
+    } else if(phantomDetect >= 2 && lineCount == 3) { // End Timed run
+      // ignore two right sensors, delay to check
+      phantomDetect = 0;
+      lineCount += 1;
+      photoWeight[0] = 0;
+      photoWeight[1] = 0;
+      photoWeight[2] = -12;
+      photoWeight[3] = -10;
+      photoWeight[4] = 10;
+      photoWeight[5] = 12;
+      photoWeight[6] = 14;
+      photoWeight[7] = 18;
+      digitalWrite(yellowled, LOW);
+      baseSpeed = 50;
+    } else if(phantomDetect >= 2 && lineCount == 3) { // End Timed run
+      // Stop CAR
+      break;
+    }
+      return; // next loop iteration will enter the "turning" block
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+bool isOnLine() {
+	if (sensorValues[0] > 2450 && sensorValues[1] > 2450  && sensorValues[2] > 2450  && sensorValues[3] > 2450  && 
+  sensorValues[4] > 2450  && sensorValues[5] > 2450  && sensorValues[6] > 2450  && sensorValues[7] > 2450) {
+    return true;
+  } else {
+    return false;
+  };
+}
